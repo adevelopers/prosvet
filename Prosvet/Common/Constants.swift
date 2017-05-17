1867db985102fbc70e67245461ba909f1ce78ee9
adeveloper2015 <hudiakov@gmail.com>
2017-05-17 22:27:40 +0300

    add Constants

---
+++ Prosvet/Common/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Prosvet
+//
+//  Created by adeveloper on 17.05.17.
+//  Copyright © 2017 adeveloper. All rights reserved.
+//
+
+import Foundation
+
+
+class Constants {
+    
+    static let baseUrlApi = "http://kultprosvetmag.ru/api/v1/"
+}
