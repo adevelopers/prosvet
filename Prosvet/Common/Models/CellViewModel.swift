27dbcd5e2bbfd8c9081ef72e25e57a9be8ca4736
adeveloper2015 <hudiakov@gmail.com>
2017-05-21 14:24:55 +0300

    add abstraction CellViewModel

---
+++ Prosvet/Common/Models/CellViewModel.swift
@@ -0,0 +1,56 @@
+//
+//  CellViewModel.swift
+//  Prosvet
+//
+//  Created by adeveloper on 21.05.17.
+//  Copyright © 2017 adeveloper. All rights reserved.
+//
+
+import UIKit
+
+protocol CellViewAnyModel {
+    static var cellAnyType: UIView.Type { get }
+    func setupAny(cell: UIView)
+}
+
+
+protocol CellViewModel: CellViewAnyModel {
+    associatedtype CellType: UIView
+    func setup(cell: CellType)
+}
+
+extension CellViewModel {
+    static var cellAnyType: UIView.Type {
+        return CellType.self
+    }
+    
+    func setupAny(cell: UIView) {
+        if let cell = cell as? CellType {
+            setup(cell: cell)
+        }
+        else {
+            assertionFailure("Wrong usage")
+        }
+    }
+}
+
+extension UITableView {
+    func dequeueReusableCell(withModel model: CellViewAnyModel, for indexPath: IndexPath) -> UITableViewCell {
+        let indetifier = String(describing: type(of: model).cellAnyType)
+        let cell = self.dequeueReusableCell(withIdentifier: indetifier, for: indexPath)
+        
+        model.setupAny(cell: cell)
+        return cell
+    }
+    
+    func register(nibModels: [CellViewAnyModel.Type]) {
+        for model in nibModels {
+            let identifier = String(describing: model.cellAnyType)
+            let nib = UINib(nibName: identifier, bundle: nil)
+            self.register(nib, forCellReuseIdentifier: identifier)
+        }
+    }
+    
+}
+
+
